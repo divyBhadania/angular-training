7b5c5fd2ef4b912551d2082d2d2d977a339e96fd
divyBhadania <divy.bhadania@bacancy.com>
2023-04-20 14:44:05 +0530

    user ngfor structural directives to render cards

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,12 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-  public card = {
-    title : "hello",
-    text : "here",
-    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntl6dP1vgF2__zJwnTAK-uUu5U_24LaSymmzXJ-Ty8BEhNNIiYjxMymk18f6LdhHSs30&usqp=CAU"
-  }
+  public cards = [
+    {
+      title: "Harry Potter and the Philosopher's Stone",
+      author: 'JKRowling',
+      text: 'here',
+      price : "199.99",
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntl6dP1vgF2__zJwnTAK-uUu5U_24LaSymmzXJ-Ty8BEhNNIiYjxMymk18f6LdhHSs30&usqp=CAU',
+    },
+    {
+      title: "The Ink Black Heart",
+      author: 'JKRowling',
+      text: 'here',
+      price : "199.99",
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntl6dP1vgF2__zJwnTAK-uUu5U_24LaSymmzXJ-Ty8BEhNNIiYjxMymk18f6LdhHSs30&usqp=CAU',
+    },
+    {
+      title: "Hamlet",
+      author: 'WilliamShakespeare',
+      text: 'here',
+      price : "199.99",
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntl6dP1vgF2__zJwnTAK-uUu5U_24LaSymmzXJ-Ty8BEhNNIiYjxMymk18f6LdhHSs30&usqp=CAU',
+    },
+    {
+      title: "Julius Caesar",
+      author: 'WilliamShakespeare',
+      text: 'here',
+      price : "199.99",
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntl6dP1vgF2__zJwnTAK-uUu5U_24LaSymmzXJ-Ty8BEhNNIiYjxMymk18f6LdhHSs30&usqp=CAU',
+    },
+    {
+      title: "The Sisters",
+      author: 'JamesJoyce',
+      text: 'here',
+      price : "199.99",
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTntl6dP1vgF2__zJwnTAK-uUu5U_24LaSymmzXJ-Ty8BEhNNIiYjxMymk18f6LdhHSs30&usqp=CAU',
+    },
+  ];
+
+  authorFilter = "";
 }
